912a9efc4de45b9d2c34c5e22698ce25e1521d7f
Kumar Navneet <kumarnavneet@Kumars-iMac.local>
2025-09-28 01:37:11 +0530

    10

--- HelloSearchService/Program.cs
+++ HelloSearchService/Program.cs
@@ -1,7 +1,7 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello from navneet first deployment");
+app.MapGet("/", () => "Hello from navneet first deployment1");
 app.MapGet("/health", () => Results.Ok("Healthy"));
 
 app.Run();
